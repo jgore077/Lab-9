4a13ef519929d28b733c629ffd77b6a8e384a7f6
jgore077 <jgore077@gmail.com>
2023-04-05 10:12:59 -0400

    update

--- StudentMovies.java
+++ StudentMovies.java
@@ -1,5 +1,8 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 class StudentMovies{
-    
+    TreeMap<String,TreeSet<String>> studentsMovies = new  TreeMap<String,TreeSet<String>>();
     public StudentMovies(){
         
     }
